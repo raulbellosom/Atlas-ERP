a8f282e75074db076bdc384a598ae839b5ff98d0
Raul Belloso Medina <raul.belloso.m@gmail.com>
2026-04-23 19:20:43 -0600

    feat(guards): add module install key metadata constant

--- apps/api/src/common/constants/authorization.constants.ts
+++ apps/api/src/common/constants/authorization.constants.ts
@@ -1,3 +1,4 @@
 export const PERMISSIONS_ALL_KEY = 'requiredPermissionsAll';
 export const PERMISSIONS_ANY_KEY = 'requiredPermissionsAny';
 export const ROLES_KEY = 'requiredRoles';
+export const MODULE_INSTALL_KEY = 'requiredModuleInstalled';
